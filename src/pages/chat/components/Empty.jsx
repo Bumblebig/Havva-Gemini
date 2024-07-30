832a532929c089e35362a4e67544dbaa13d0ae48
Abbdurahman <dreydare16@gmail.com>
2024-07-30 18:45:00 +0100

    adjusted dummy text width

--- src/pages/chat/components/Empty.jsx
+++ src/pages/chat/components/Empty.jsx
@@ -6,11 +6,11 @@ const Empty = function () {
         <section className="pt-14 text-gray-900">
             <img src={bot} alt="Havva" className="w-24 rounded-full mx-auto" />
             <h1 className="text-center font-bold text-3xl mt-8 mb-12 text-primary">Havva</h1>
-            <div className="flex items-stretch gap-8 max-w-[90%] mx-auto mt-6">
+            <div className="flex items-stretch gap-8 max-w-[90%] mx-auto mt-6 w-fit">
                 <DefaultPrompt text="I need first aid advice for an emergency." />
                 <DefaultPrompt text="Can you suggest treatments for my condition?" />
             </div>
-            <div className="flex items-stretch gap-8 max-w-[90%] mx-auto mt-6">
+            <div className="flex items-stretch gap-8 max-w-[90%] mx-auto mt-6 w-fit">
                 <DefaultPrompt text="What over-the-counter medication should I take?" />
                 <DefaultPrompt text="How can I learn more about my health condition?" />
             </div>
