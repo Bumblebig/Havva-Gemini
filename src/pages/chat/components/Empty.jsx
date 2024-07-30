0e3ae9fadc7458ca9c040c7257eaeb5bc558c86c
Abbdurahman <dreydare16@gmail.com>
2024-07-30 18:38:49 +0100

    updated dummy text

--- src/pages/chat/components/Empty.jsx
+++ src/pages/chat/components/Empty.jsx
@@ -7,12 +7,12 @@ const Empty = function () {
             <img src={bot} alt="Havva" className="w-24 rounded-full mx-auto" />
             <h1 className="text-center font-bold text-3xl mt-8 mb-12 text-primary">Havva</h1>
             <div className="flex items-stretch gap-8 max-w-[90%] mx-auto mt-6">
-                <DefaultPrompt />
-                <DefaultPrompt />
+                <DefaultPrompt text="I need first aid advice for an emergency." />
+                <DefaultPrompt text="Can you suggest treatments for my condition?" />
             </div>
             <div className="flex items-stretch gap-8 max-w-[90%] mx-auto mt-6">
-                <DefaultPrompt />
-                <DefaultPrompt />
+                <DefaultPrompt text="What over-the-counter medication should I take?" />
+                <DefaultPrompt text="How can I learn more about my health condition?" />
             </div>
         </section>
     )
