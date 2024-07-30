d305142ba4d0a174fd3ec7264869627e6f50f354
Abbdurahman <dreydare16@gmail.com>
2024-07-30 18:42:52 +0100

    adjusted dummy text width

--- src/pages/chat/components/DefaultPrompt.jsx
+++ src/pages/chat/components/DefaultPrompt.jsx
@@ -1,6 +1,6 @@
 const DefaultPrompt = ({ text }) => {
     return (
-        <div className="border border-gray-500 rounded p-4 cursor-pointer">
+        <div className="border border-gray-500 rounded p-4 cursor-pointer w-[360px] max-w-[360px]">
             <p className="leading-snug text-sm">{text}</p>
         </div>
     )
